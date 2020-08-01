804bc636916818a44386424780ae837a474fda34
Kenneth Ofosu <kofosu89@gmail.com>
2020-08-01 00:21:52 -0400

    production deploy

--- public/path----131f9aef94525eeb6ecd.js
+++ public/path----131f9aef94525eeb6ecd.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{106:function(t,o){t.exports={layoutContext:{}}},107:function(t,o){t.exports={layoutContext:{}}}});
+webpackJsonp([60335399758886],{107:function(t,o){t.exports={layoutContext:{}}},108:function(t,o){t.exports={layoutContext:{}}}});
 //# sourceMappingURL=path----131f9aef94525eeb6ecd.js.map
